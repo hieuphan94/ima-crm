97ee62dba063ec3b970181e09830f816936f6c79
hieuphan94 <phantronghieu1@gmail.com>
2025-02-18 09:42:42 +0700

    Cập nhật ảo

--- services/backend/src/routes/index.js
+++ services/backend/src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const userRoutes = require('./userRoutes');
-const tourRoutes = require('./tourRoutes');
+// const tourRoutes = require('./tourRoutes');
 const serviceCategoryRoutes = require('./serviceCategoryRoutes');
 const dayTemplateRoutes = require('./dayTemplateRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
-router.use('/tours', tourRoutes);
+// router.use('/tours', tourRoutes);
 router.use('/service-categories', serviceCategoryRoutes);
 router.use('/day-templates', dayTemplateRoutes);
 
